7b147b74c9664adaf117be6b4b988e1fc1c97ca3
ArunNHI <arun@microsoftonline.com>
2016-12-08 20:31:24 +0000

    FirstChange

---
+++ MyfirstProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyfirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Welcome to series of Learning C Sharp 
+             * My name is Arun Mandhata
+             * I am a Microsoft certified Trainer
+             * Teaching C#.Net, Microsft SQL Server,SharePoint and CRM
+             *  */
+            string msg = "I want to learn C#";
+            Console.WriteLine(msg);
+            Console.WriteLine("Press any key to end program");
+            Console.Read();
+        }
+    }
+}
