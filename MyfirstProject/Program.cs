2584218f3d6638107e98b27b55737c20b51a1e8e
ArunNHI <arun@microsoftonline.com>
2016-12-08 20:52:44 +0000

    Questions in mind

--- MyfirstProject/Program.cs
+++ MyfirstProject/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,10 +19,31 @@ namespace MyfirstProject
              * Teaching C#.Net,Microsft SQL Server,SharePoint and CRM
              *  */
 
-            string msg = "I want to learn C#";
-            Console.WriteLine(msg);
-            Console.WriteLine("Press any key to end program");
-            Console.Read();
+            string msgs = "I want to learn C#";
+            Console.WriteLine(msgs );
+            Console.WriteLine("Press any key to end program....");
+            Console.ReadKey();
+            // Our first program can bring this questions in the mind of learner
+            //You can add if you have any more question
+//Q1 What is using System
+//Q2 Why do I see Green and blue text
+//Q3 Why do we put Semi Colon ";" sign at end
+//Q4 What is Class 
+//Q5 What is Program and why its in Blue color
+//Q6 What is Static
+//Q7 What is Void
+//Q8 What is Main()
+//Q9What is String[] args
+//Q10 What is Console
+//Q11 What is WriteLine
+//Q12 Why do we put Dot after Console
+//Q13 What is Read()
+//Q14 Why do we put it at end
+//Q15 What is curly braces "{} "
+//Q16 what is /* */
+//Q17 What is "//"
+//Q19 Why do we put "" when we wrote I want to learn C#
+// This all answer of such questions will be given in further part of this tutorial have pataience and keep learning.
         }
     }
 }
